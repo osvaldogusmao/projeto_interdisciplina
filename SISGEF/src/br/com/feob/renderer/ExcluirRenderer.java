98c2f19f898a7eaabddf5d70e82be83de7afa22d
José Osvaldo Gusmão <osvaldogusmao@yahoo.com.br>
2012-04-08 20:42:13 -0300

    Projeto - Diretorios

---
+++ SISGEF/src/br/com/feob/renderer/ExcluirRenderer.java
@@ -0,0 +1,14 @@
+package br.com.feob.renderer;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class ExcluirRenderer extends JFrame{
+
+	public ExcluirRenderer() {
+		
+		JButton btnExcluir = new JButton("Excluir");
+		
+	}
+	
+}
